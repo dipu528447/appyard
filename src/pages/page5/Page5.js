db53d8bcd3363034af6713723ecb4b84ecad4795
Dipankar Gupta <46720652+dipu528447@users.noreply.github.com>
2023-02-15 20:32:49 +0600

    complete flow 5

--- src/pages/page5/Page5.js
+++ src/pages/page5/Page5.js
@@ -11,17 +11,28 @@ const Page5 = () => {
                 whileInView={{
                     opacity:1,
                 }}
-                transition={{ delay:.5,duration:.6}}
+                transition={{ delay:0,duration:.6}}
                 viewport={{once:true}}
                 style={{width:"1200px"}}
                 >
                 <h1 style={{fontFamily:'Inter',fontWeight:"700", marginBottom:"100px"}}>MONITOR YOUR <span style={{color:"#8AB6CB"}}>PROJECT’S</span> PROGRESS</h1>
                 <div style={{display:"flex", justifyContent:"space-between"}}>
                     <div style={{width:"500px"}}>
-                        <h1>Enter Your Customer ID</h1>
-                        <div class="slide-right">
-                            <input type="text" className="textfield"></input>
-                        </div>
+                        
+                            <h1>Enter Your Customer ID</h1>
+                            <div class="slide-right">
+                                <motion.input
+                                initial={{width:"0px"}}
+                                whileInView={{
+                                    width:"360px"
+                                }}
+                                transition={{ delay:0,duration:.6}}
+                                viewport={{once:true}}
+                                type="text" 
+                                style={{height: "50px",padding:"0px 10px",color: "black",fontSize: "20px",backgroundColor: "white",width:"350px" }} >
+                                </motion.input>
+                                <button className='btnTrack'>Track Your Progress</button>
+                            </div>
                         
                     </div>
                     <div>
